5847863f2ae92eda5d8b972d915941912175754c
Quest Henkart <qhenkart@gmail.com>
2014-02-17 19:40:28 -0800

    initialize repository

---
+++ hartlsampleapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hartlsampleapp::Application.config.secret_key_base = 'c644f828fa9c990eee0810377a806dfe054644dd13a4c0d4ee6d63368e067fa9a79ba7724a3e115527e54754e33960e7d88609eb0cbf551c8509c68311a1681d'
---
+++ hartlsampleapp/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hartlsampleapp::Application.config.session_store :cookie_store, key: '_hartlsampleapp_session'
